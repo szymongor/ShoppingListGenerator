c2188f84779c5346b924a7e1da56ed163283cc0b
sgornioczek <szymon.gornioczek@agora.pl>
2022-07-18 20:39:42 +0200

    Discovery and Config service

--- recipes-ms/src/test/java/com/szymongor/recipesms/RecipesMsApplicationTests.java
+++ recipes-ms/src/test/java/com/szymongor/recipesms/RecipesMsApplicationTests.java
@@ -2,8 +2,10 @@ package com.szymongor.recipesms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = { "spring.cloud.config.debug: true", "port: 4200" })
 class RecipesMsApplicationTests {
 
 	@Test
